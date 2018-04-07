44a945acbe2d85fbd546b4b0e3818f93814e6d77
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2018-04-07 12:20:12 +0200

    include main name into search (important)

--- FrontendServer/WikidataEntity.cpp
+++ FrontendServer/WikidataEntity.cpp
@@ -6,6 +6,9 @@ WikidataEntity::WikidataEntity(const std::string& line) {
   auto pos = line.find("\t");
   name = line.substr(0, pos);
   auto newpos = line.find("\t", pos + 1);
+  // the name is also an alias, and since this is only used
+  // during initialization, the duplicate does not matter
+  aliases.push_back(name);
   while (newpos != std::string::npos) {
     auto alias = line.substr(pos + 1, newpos - pos);
     aliases.push_back(alias);
