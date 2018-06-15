61699c3bb431f0ab65467ac0388e1e5da3db7e0e
Johannes <johannes.kalmbach@gmail.com>
2018-06-15 15:40:36 +0200

    Removed boost dependency from VocabularyGeneratorTest
    - travis does not have boost
    - hardcoded /tmp/ directory (should work on all linuxes)

--- test/VocabularyGeneratorTest.cpp
+++ test/VocabularyGeneratorTest.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <fstream>
-#include <boost/filesystem.hpp>
-#include <boost/iostreams/device/mapped_file.hpp>
+#include <cstdlib>
+#include <ctime>
 
 #include "../src/index/VocabularyGenerator.h"
 #include "../src/global/Constants.h"
@@ -13,43 +13,44 @@ class MergeVocabularyTest : public ::testing::Test {
 protected:
 
   // path of the 2 partial Vocabularies that are used by mergeVocabulary
-  boost::filesystem::path _path0;
-  boost::filesystem::path _path1;
+  std::string _path0;
+  std::string _path1;
   // path of the 2 partial vocabularies that are the expected output of
   // mergeVocabulary
-  boost::filesystem::path _pathExp0;
-  boost::filesystem::path _pathExp1;
+  std::string _pathExp0;
+  std::string _pathExp1;
   // the base directory for our test
-  boost::filesystem::path _basePath;
+  std::string _basePath;
   // path to expected vocabulary text file
-  boost::filesystem::path _pathVocabExp;
+  std::string _pathVocabExp;
   // path to expected external vocabulary text file
-  boost::filesystem::path _pathExternalVocabExp;
+  std::string _pathExternalVocabExp;
 
   // Constructor. TODO: Better write Setup method because of complex logic which
   // may throw?
   MergeVocabularyTest() {
     // name of random subdirectory
-    _basePath = boost::filesystem::unique_path("qleverVocTest%%%%_%%%%_%%%%_%%%%");
+    std::srand(std::time(nullptr));
+    _basePath = std::string("qleverVocTest-") + std::to_string(std::rand());
     // those names are required by mergeVocabulary
-    _path0 = boost::filesystem::path(PARTIAL_VOCAB_FILE_NAME + std::to_string(0));
-    _path1 =  boost::filesystem::path(PARTIAL_VOCAB_FILE_NAME + std::to_string(1));
+    _path0 = std::string(PARTIAL_VOCAB_FILE_NAME + std::to_string(0));
+    _path1 =  std::string(PARTIAL_VOCAB_FILE_NAME + std::to_string(1));
     // those names can be random
-    _pathExp0 = boost::filesystem::path(".partialVocabExp0");
-    _pathExp1  = boost::filesystem::path(".partialVocabExp1");
+    _pathExp0 = std::string(".partialVocabExp0");
+    _pathExp1  = std::string(".partialVocabExp1");
 
     // create random subdirectory in /tmp
-    auto tempPath = boost::filesystem::temp_directory_path();
-    _basePath = tempPath / _basePath;
-    boost::filesystem::create_directory(_basePath);
+    std::string tempPath = "/tmp/";
+    _basePath = tempPath + _basePath + "/";
+    system(("mkdir -p " + _basePath).c_str());
 
     // make paths abolute under created tmp directory
-    _path0 = _basePath / _path0;
-    _path1 = _basePath / _path1;
-    _pathExp0 = _basePath / _pathExp0;
-    _pathExp1 = _basePath / _pathExp1;
-    _pathVocabExp  = _basePath / boost::filesystem::path(".vocabExp");
-    _pathExternalVocabExp = _basePath / boost::filesystem::path("externalTextFileExp");
+    _path0 = _basePath + _path0;
+    _path1 = _basePath + _path1;
+    _pathExp0 = _basePath + _pathExp0;
+    _pathExp1 = _basePath + _pathExp1;
+    _pathVocabExp  = _basePath + std::string(".vocabExp");
+    _pathExternalVocabExp = _basePath + std::string("externalTextFileExp");
 
 
     // these will be the contents of partial vocabularies, second element of
@@ -58,18 +59,18 @@ protected:
     std::vector<std::pair<std::string, size_t>> words2{{"bear", 1}, {"monkey", 3}, {"zebra", 4}};
 
     // write expected vocabulary files
-    std::ofstream expVoc(_pathVocabExp.string());
-    std::ofstream expExtVoc(_pathExternalVocabExp.string());
+    std::ofstream expVoc(_pathVocabExp);
+    std::ofstream expExtVoc(_pathExternalVocabExp);
     expVoc << "ape\nbear\ngorilla\nmonkey\nzebra\n";
     expExtVoc << EXTERNALIZED_LITERALS_PREFIX << "bla\n";
    
 
     // open files for partial Vocabularies
     auto mode = std::ios_base::out | std::ios_base::binary;
-    std::ofstream partial0(_path0.string(), mode);
-    std::ofstream partial1(_path1.string(), mode);
-    std::ofstream partialExp0(_pathExp0.string(), mode);
-    std::ofstream partialExp1(_pathExp1.string(), mode);
+    std::ofstream partial0(_path0, mode);
+    std::ofstream partial1(_path1, mode);
+    std::ofstream partialExp0(_pathExp0, mode);
+    std::ofstream partialExp1(_pathExp1, mode);
 
     if (! partial0.is_open()) std::cerr << "could not open temp file at" << _path0 << '\n';
     if (! partial1.is_open()) std::cerr << "could not open temp file at" << _path1 << '\n';
@@ -126,24 +127,44 @@ protected:
   // returns true if and only if the files with names n1 and n2 exist, can be
   // opened for reading and are bytewise equal.
   bool areBinaryFilesEqual(const std::string& n1, const std::string& n2) {
-    namespace io = boost::iostreams;
-    io::mapped_file_source f1(n1);
-    io::mapped_file_source f2(n2);
+    auto p1 = readAllBytes(n1);
+    auto p2 = readAllBytes(n2);
+    auto s1 = p1.first;
+    auto s2 = p2.first;
+    auto& f1 = p1.second;
+    auto& f2 = p2.second;
 
-    return (f1.is_open() && f2.is_open() && f1.size() == f2.size() && std::equal(f1.data(), f1.data() + f1.size(), f2.data()));
+    return (s1 && s2 && f1.size() == f2.size() && std::equal(f1.begin(), f1.end() , f2.begin()));
   }
 
+  // read all bytes from a file (e.g. to check equality of small test files)
+  static std::pair<bool, std::vector<char>> readAllBytes(const std::string& filename)
+  {
+      using std::ifstream;
+      ifstream ifs(filename, std::ios::binary | std::ios::ate);
+      if (!ifs.is_open()) {
+	return std::make_pair(false, std::vector<char>());
+      }
+      ifstream::pos_type pos = ifs.tellg();
+
+      std::vector<char>  result(pos);
+
+      ifs.seekg(0, std::ios::beg);
+      ifs.read(&result[0], pos);
+
+      return std::make_pair(true, result);
+  }
 };
 
 // Test for merge Vocabulary
 TEST_F(MergeVocabularyTest, bla) {
 
   // mergeVocabulary only gets name of directory and number of files.
-  mergeVocabulary(_basePath.string() + '/', 2);
+  mergeVocabulary(_basePath , 2);
   // Assert that partial vocabularies have the expected ids
-  ASSERT_TRUE(areBinaryFilesEqual(_path0.string(), _pathExp0.string()));
-  ASSERT_TRUE(areBinaryFilesEqual(_path1.string(), _pathExp1.string()));
+  ASSERT_TRUE(areBinaryFilesEqual(_path0, _pathExp0));
+  ASSERT_TRUE(areBinaryFilesEqual(_path1, _pathExp1));
   // check that (external) vocabulary has the right form.
-  ASSERT_TRUE(areBinaryFilesEqual(_pathVocabExp.string(), _basePath.string() + "/.vocabulary"));
-  ASSERT_TRUE(areBinaryFilesEqual(_pathExternalVocabExp.string(), _basePath.string() + "/.externalTextFile"));
+  ASSERT_TRUE(areBinaryFilesEqual(_pathVocabExp, _basePath + ".vocabulary"));
+  ASSERT_TRUE(areBinaryFilesEqual(_pathExternalVocabExp, _basePath + ".externalTextFile"));
 }
